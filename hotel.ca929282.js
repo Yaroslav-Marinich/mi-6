e4b82280e0b539439dca48a1e167317f908d0e77
Yaroslav-Marinich <113300647+Yaroslav-Marinich@users.noreply.github.com>
2022-12-09 12:38:14 +0000

    Deploying to gh-pages from @ Yaroslav-Marinich/mi-6@72a0a68fa82ce6607d6c665e6a1e1aa1c86562db 🚀

---
+++ hotel.ca929282.js
@@ -0,0 +1,2 @@
+$(".autoplay").slick({slidesToShow:3,slidesToScroll:1,autoplay:!0,autoplaySpeed:2e3}),$(".fade").slick({dots:!0,infinite:!0,speed:500,fade:!0,cssEase:"linear"});
+//# sourceMappingURL=hotel.ca929282.js.map
