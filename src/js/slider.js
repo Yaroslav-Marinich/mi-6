f0f0cd194e876946139ec06cee29271391998387
YevgeniyaAndriyevskaya <e.andrievskaja2010@gmail.com>
2022-12-11 22:31:03 +0200

    finished slick slider, workin on slide buttons

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,20 +1,27 @@
 // SMALL GALLERIES SLIDER - SLICK SCRIPT
 
 $('.autoplay').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-  });
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
 
-
-  // ROOMS PAGE SLIDER - SLICK SCRIPT
+// ROOMS PAGE SLIDER - SLICK SCRIPT
 
 $('.fade').slick({
   dots: true,
   infinite: true,
   speed: 500,
   fade: true,
-  cssEase: 'linear'
+  cssEase: 'linear',
 });
-			
+
+// Restaurant page -> Kitchen Section -> Slider
+$('.kitchen-slider').slick({
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  variableWidth: true,
+  autoplay: false,
+  autoplaySpeed: 1000,
+});
