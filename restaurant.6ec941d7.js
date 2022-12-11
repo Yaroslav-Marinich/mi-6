618df632d3dd9924edea70854a48dc7575b2636f
Yaroslav-Marinich <113300647+Yaroslav-Marinich@users.noreply.github.com>
2022-12-11 09:15:08 +0000

    Deploying to gh-pages from @ Yaroslav-Marinich/mi-6@0dec3b3dc992488e4517c8e680bca56e74dd60b3 🚀

---
+++ restaurant.6ec941d7.js
@@ -0,0 +1,2 @@
+$(".autoplay").slick({slidesToShow:3,slidesToScroll:1,autoplay:!0,autoplaySpeed:2e3}),$(".fade").slick({dots:!0,infinite:!0,speed:500,fade:!0,cssEase:"linear"}),$(".variable-width").slick({dots:!1,infinite:!0,speed:300,slidesToShow:1,centerMode:!0,variableWidth:!0,autoplay:!0,autoplaySpeed:3e3}),$(".autoplay-mini").slick({slidesToShow:1.1,slidesToScroll:1,variableWidth:!0,autoplay:!0,autoplaySpeed:2e3});
+//# sourceMappingURL=restaurant.6ec941d7.js.map
--- restaurant.92353aaa.js
+++
@@ -1,2 +0,0 @@
-$(".autoplay").slick({slidesToShow:3,slidesToScroll:1,autoplay:!0,autoplaySpeed:2e3}),$(".fade").slick({dots:!0,infinite:!0,speed:500,fade:!0,cssEase:"linear"});
-//# sourceMappingURL=restaurant.92353aaa.js.map
