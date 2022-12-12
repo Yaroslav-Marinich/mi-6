69fb7fae324e548a9bdeb6d60e701bf27da5844f
Stanislav Dmytriyev <e.andrievskaja2010@gmail.com>
2022-12-12 21:01:46 +0200

    corrections to slider.js to align with main branch

--- src/js/slider.js
+++ src/js/slider.js
@@ -17,6 +17,28 @@ $('.fade').slick({
   cssEase: 'linear',
 });
 
+// SLICK SLIDER FOR VARIABLE WIDTH
+
+$('.variable-width').slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  initialSlide: 0,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  variableWidth: true,
+});
+
+// SLICK-SLIDER Mini
+
+$('.autoplay-mini').slick({
+  slidesToShow: 1.1,
+  slidesToScroll: 1,
+  variableWidth: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+
 // Restaurant page -> Kitchen Section -> Slider
 $('.kitchen-slider').slick({
   slidesToShow: 2,
